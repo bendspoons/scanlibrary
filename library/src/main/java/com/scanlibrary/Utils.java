68f4eb084f3256717d9fbf2bd97e5b931c541814
Dominic Roesmann <dominic.roesmann@googlemail.com>
2020-03-02 21:18:08 +0100

    Update Utils.java
    Changed to (hopefully) resolve /Huawei) Android 10 issue

--- library/src/main/java/com/scanlibrary/Utils.java
+++ library/src/main/java/com/scanlibrary/Utils.java
@@ -1,6 +1,7 @@
 package com.scanlibrary;
 
 import android.content.Context;
+import android.content.ContentValues;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -20,8 +21,16 @@ public class Utils {
     public static Uri getUri(Context context, Bitmap bitmap) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
-        return Uri.parse(path);
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"Title");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From Camera");
+        Uri path = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        return path;
+
+        //String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
+        //return Uri.parse(path);
     }
 
     public static Bitmap getBitmap(Context context, Uri uri) throws IOException {
